20728c09dda320f4aed17ee33ec4beac69be6747
kattbcc17 <kattbcc17@byui.edu>
2024-09-21 17:06:09 -0500

    lesson 1

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 // express web server
 var express = require('express');
 const app = express();
+const lesson1Controller = require('./controllers/lesson1');
 
-app.get('/', (req, res) => {
-    res.send('Sarah Birch');
-})
+app.get('/', lesson1Controller.emilyRoute);
+app.get('/hannah', lesson1Controller.hannahRoute);
 
 const port = 3000;
 
 app.listen(process.env.port || port); 
-console.log('Web server is listening at port ' + (process.env.port || 3000));
+console.log('Web server is listening at port ' + (process.env.port || port));
